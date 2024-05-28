76e35ea4f3af147f3271d02558f0ae3fd9a5f903
lons <lonsnw@gmail.com>
2024-05-28 17:48:46 -0500

    Built out logo bar

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,8 +1,26 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './Footer.css';
 
+// MUI imports
+import { Icon } from '@mui/material';
+
 function Footer() {
-  return <footer>&copy; 2024 Lons Nadziejka Waller</footer>;
+
+  const logoIcon = (
+    <Icon style={{ fontSize: 80 }} >
+      <img src="../../../public/images/skates.svg" alt="logo" />
+    </Icon>
+  );
+
+  return <footer>
+    <div className="logo">
+      <Link to="/home">
+        {logoIcon}
+      </Link>
+    </div>
+      &copy; 2024 Lons Nadziejka Waller
+    </footer>;
 }
 
 export default Footer;
