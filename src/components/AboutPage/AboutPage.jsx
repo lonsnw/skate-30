75732a3f66aca9ba99d46891a34406c2c8bfbce2
lons <lonsnw@gmail.com>
2024-05-28 13:36:34 -0500

    Making small notes and correcting typos

--- src/components/AboutPage/AboutPage.jsx
+++ src/components/AboutPage/AboutPage.jsx
@@ -1,9 +1,6 @@
 import React from 'react';
 
-// This is one of our simplest components
-// It doesn't have local state,
-// It doesn't dispatch any redux actions or display any part of redux state
-// or even care what the redux state is'
+// Add an about page for presentation, etc.
 
 function AboutPage() {
   return (
