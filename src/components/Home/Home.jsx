5ef497a684d04d6dc7ebf745af471dff56ea33ed
lons <lonsnw@gmail.com>
2024-05-29 08:58:23 -0500

    Removed Login code from Home

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -2,6 +2,9 @@ import React, { useState } from 'react';
 import { useHistory } from 'react-router-dom';
 import './Home.css';
 
+// MUI imports
+import { Container } from '@mui/material';
+
 // This will become Home 
 // CUSTOM COMPONENTS
 import RegisterForm from '../RegisterForm/RegisterForm';
@@ -9,22 +12,14 @@ import RegisterForm from '../RegisterForm/RegisterForm';
 function Home() {
   const history = useHistory();
 
-  const onLogin = (event) => {
-    history.push('/login');
-  };
-
   return (
     <div className="hero">
+
       <h1>Find  ice times near you</h1>
       <h5>Search for local pickups and open skates</h5>
         <div className="grid-col grid-col_4">
-          <RegisterForm />
 
           <center>
-            <h4>Already a Member?</h4>
-            <button className="btn btn_sizeSm" onClick={onLogin}>
-              Login
-            </button>
           </center>
         </div>
       </div>
