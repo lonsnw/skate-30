ad0ef8195203866cac6d920459f6a940e7e2e1f1
lons <lonsnw@gmail.com>
2024-05-29 09:34:46 -0500

    Moved search bar to its own component, SearchBar

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,5 +1,4 @@
 import React, { useState } from 'react';
-import { useHistory } from 'react-router-dom';
 import './Home.css';
 
 // MUI imports
@@ -7,12 +6,10 @@ import { Box, Button, Container, Divider, IconButton, InputBase, Paper, Typograp
 import MenuIcon from '@mui/icons-material/Menu';
 import SearchIcon from '@mui/icons-material/Search';
 
-// This will become Home 
 // CUSTOM COMPONENTS
-import RegisterForm from '../RegisterForm/RegisterForm';
+import SearchBar from '../SearchBar/SearchBar';
 
 function Home() {
-  const history = useHistory();
 
   return (
     <div className="hero">
@@ -25,25 +22,7 @@ function Home() {
           marginTop='20px' >
           <Typography variant="h3">Find  ice times near you</Typography>
           <Typography variant="h5">Search for local pickups and open skates</Typography>
-          <div className="search-bar">
-            <Paper
-              component="form"
-              sx={{ p: '2px 4px', display: 'flex', alignItems: 'center' }}
-            >
-              <IconButton sx={{ p: '10px' }} aria-label="menu">
-                <MenuIcon />
-              </IconButton>
-              <InputBase
-                sx={{ ml: 1, flex: 1 }}
-                placeholder="Search for ice"
-                inputProps={{ 'aria-label': 'search google maps' }}
-              />
-              <Divider sx={{ height: 28, m: 0.5 }} orientation="vertical" />
-              <IconButton type="button" sx={{ p: '10px' }} aria-label="search">
-                <SearchIcon />
-              </IconButton>
-            </Paper>
-          </div>
+          <SearchBar />
         </Box>
       </Container>
     </div>
---
+++ src/components/SearchBar/SearchBar.jsx
@@ -0,0 +1,34 @@
+import React, { useState } from 'react';
+import {useSelector} from 'react-redux';
+
+// MUI imports
+import { Divider, IconButton, InputBase, Paper } from '@mui/material';
+import MenuIcon from '@mui/icons-material/Menu';
+import SearchIcon from '@mui/icons-material/Search';
+
+function SearchBar() {
+
+  return (
+    <div className="search-bar">
+        <Paper
+        component="form"
+        sx={{ p: '2px 4px', display: 'flex', alignItems: 'center' }}
+        >
+        <IconButton sx={{ p: '10px' }} aria-label="menu">
+            <MenuIcon />
+        </IconButton>
+        <InputBase
+            sx={{ ml: 1, flex: 1 }}
+            placeholder="Search for ice"
+            inputProps={{ 'aria-label': 'search for ice' }}
+        />
+        <Divider sx={{ height: 28, m: 0.5 }} orientation="vertical" />
+        <IconButton type="button" sx={{ p: '10px' }} aria-label="search">
+            <SearchIcon />
+        </IconButton>
+        </Paper>
+    </div>    
+  );
+}
+
+export default SearchBar;
