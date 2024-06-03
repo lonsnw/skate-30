9755d80889e1e28e337891bf525aaf37938b1f8b
lons <lonsnw@gmail.com>
2024-06-03 10:52:00 -0500

    Search working

--- src/redux/reducers/search.reducer.js
+++ src/redux/reducers/search.reducer.js
@@ -2,9 +2,7 @@ import { combineReducers } from 'redux';
 
 const searchResults = (state = [], action) => {
     switch (action.type) {
-      case 'SET_RESULTS':
-        return action.payload;
-      case 'SET_SEARCH':
+      case 'SEND_SEARCH':
         return action.payload;
       default:
         return state;
--- src/redux/sagas/search.saga.js
+++ src/redux/sagas/search.saga.js
@@ -5,16 +5,16 @@ function* searchResults(action) {
     console.log('in searchResults saga');
     try {
         const searchResponse = yield axios.get(`/api/search/`, action.payload);
-        yield put ({ type: 'SET_RESULTS', payload: searchResponse.data })
+        yield put ({ type: 'SEND_SEARCH', payload: searchResponse.data })
     } catch {
         console.log('Error with searchResults saga');
     }
 }
 
-function* sendSearch(action) {
-    console.log('in sendSearch saga');
-    yield put ({ type: 'SET_SEARCH', payload: action.payload })
-}
+// function* sendSearch(action) {
+//     console.log('in sendSearch saga');
+//     yield put ({ type: 'SET_SEARCH', payload: action.payload })
+// }
 
 function* searchSaga() {
     yield takeLatest('SEARCH_RESULTS', searchResults);
