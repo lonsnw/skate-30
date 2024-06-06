7a5496f402fc336f0ca61ad8b0ac51494d193554
lons <lonsnw@gmail.com>
2024-06-05 19:10:57 -0500

    Tidying LoginPage and duplicating LoginPage styling/etc. for RegisterPage

--- src/components/LoginPage/LoginPage.jsx
+++ src/components/LoginPage/LoginPage.jsx
@@ -1,16 +1,12 @@
 import React from 'react';
-import LoginForm from '../LoginForm/LoginForm';
-import { useHistory } from 'react-router-dom';
 
 // MUI imports
-import { Box, Container, Paper } from '@mui/material';
-import { styled } from '@mui/material/styles';
+import { Box, Container } from '@mui/material';
 
 // CUSTOM COMPONENTS
-import Footer from '../Footer/Footer'
+import LoginForm from '../LoginForm/LoginForm';
 
 function LoginPage() {
-  const history = useHistory();
 
   return (
     <div className="hero">
