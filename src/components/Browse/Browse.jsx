6e26d977a458433d148b07ca6061b9b002261488
lons <lonsnw@gmail.com>
2024-06-07 07:36:08 -0500

    Added conditional to browse to show options if no events populate

--- src/components/Browse/Browse.jsx
+++ src/components/Browse/Browse.jsx
@@ -1,8 +1,9 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import { useHistory } from 'react-router-dom';
 
 // MUI imports
-import { Box, Grid, Paper } from '@mui/material';
+import { Box, Button, Grid, Paper, Typography } from '@mui/material';
 import { styled } from '@mui/material/styles';
 
 // CUSTOM COMPONENTS
@@ -10,8 +11,9 @@ import EventCard from '../EventCard/EventCard';
 import Footer from '../Footer/Footer'
 
 function Browse() {
-  const user = useSelector((store) => store.user);
-  const dispatch = useDispatch();
+  const events = useSelector((store) => store.browse.browseAll);
+  const history = useHistory();
+    const dispatch = useDispatch();
 
   useEffect(() => {
     dispatch({ type: 'FETCH_EVENTS' })
@@ -30,7 +32,42 @@ function Browse() {
         flexDirection='column'>
           <Grid className='events'>
               <Grid >
+              {
+                  events.length === 0 ? (
+                    <Box 
+                      sx={{ height: '74vh' }}
+                      display='flex'
+                      flexDirection='column'
+                      justifyContent='center'
+                      textAlign='center'>
+                      <Typography variant="h5">No entries were found.</Typography>
+                      <Box
+                        display='flex'
+                        flexDirection='row'
+                        justifyContent='center'
+                        gap={1}
+                        margin='10px'>
+                        <Button
+                          type="button"
+                          variant="contained"
+                          color="secondary"
+                          name="input"
+                          onClick={() => {history.push('/input');}}>
+                          Add event
+                        </Button>
+                        <Button
+                          type="button"
+                          variant="contained"
+                          name="try-again"
+                          onClick={() => {history.push('/');}}>
+                          Try again
+                        </Button>
+                      </Box>
+                    </Box>
+                  ) : (
                 <EventCard />
+                  )
+                }
               </Grid>
           </Grid>
       </Item>
