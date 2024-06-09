a3c624c9fe216492c632c20c193d75ade35cd1de
lons <lonsnw@gmail.com>
2024-06-09 11:44:10 -0500

    DOM displaying labels correctly

--- src/components/RSVP/RSVP.jsx
+++ src/components/RSVP/RSVP.jsx
@@ -3,17 +3,34 @@ import { useSelector } from 'react-redux';
 
 // MUI imports
 import { styled } from '@mui/material/styles';
-import { Box, Card, Grid, Link, Paper, Stack, Switch, Typography } from '@mui/material';
+import { Box, Card, FormControlLabel, Grid, Paper, Radio, Stack, TextField, Typography } from '@mui/material';
 
 // CUSTOM COMPONENTS
 import Footer from '../Footer/Footer';
 import GoogleMap from '../GoogleMap/GoogleMap';
 import Loading from '../Loading/Loading';
+import PlayerToggle from '../PlayerToggle/PlayerToggle'
 import RsvpToggle from '../RsvpToggle/RsvpToggle'
 
 function RSVP(){
     const details = useSelector(store => store.details.details);
     const user = useSelector((store) => store.user);
+    const [notes, setNotes] = useState();
+    const [pucks, setPucks] = useState();
+    const [tutor, setTutor] = useState();
+    const [drinks, setDrinks] = useState();
+
+    const handlePucks = () => {
+
+    };
+
+    const handleTutor = () => {
+
+    };
+
+    const handleDrinks = () => {
+
+    };
 
 // STYLING
     const Item = styled(Paper)(({ theme }) => ({
@@ -40,124 +57,46 @@ function RSVP(){
                     display='flex'
                     flexDirection='column'>
                 <Grid>
-                    <Item >
-                        {/* On load, it sometimes throws an error that .type isn't recognized; if I comment out and then let it load, it works and I can comment it back in. */}
-                        <Typography variant='h6'><li>{ details[0].type ? 'Pickup' : 'Open Skate' } - { details[0].rink }</li></Typography>
+                    <Box
+                        display='flex'
+                        flexDirection='column'
+                        alignItems='center'
+                        padding='5px'>
+                        <Typography variant='h4'>RSVP</Typography>
+                        <Typography variant='h6'>{ details[0].type ? 'Pickup' : 'Open Skate' } - { details[0].rink }</Typography>
                         <Typography variant="subtitle1">{new Date(details[0].date).toLocaleDateString('en-us', { weekday:"long", month:"short", day:"numeric"})} - {details[0].time} - {details[0].duration} mins</Typography>
-                    </Item>
-                    <Box
-                        display='flex'
-                        flexDirection='column'
-                        alignItems='center'
-                        padding='5px'>
-                        <Typography variant='h6'>RSVP</Typography>
                         <RsvpToggle eventId={details[0].id} />
+                        <PlayerToggle />
                     </Box>
-                    <Item>
-                        <Stack 
-                            direction="column" 
-                            spacing={1} 
-                            alignItems="center">
-                            <Typography variant='subtitle1'>Address</Typography>
-                            <GoogleMap />
-                            { details[0].address }
+                    <Box
+                        display='flex'
+                        flexDirection='column'
+                        alignItems='center'
+                        padding='5px'>
+                        <Typography variant='h5'>Bringing:</Typography>
+                        <Stack
+                            direction="row">
+                            <Stack 
+                                direction="column">
+                                <FormControlLabel 
+                                    value="pucks"
+                                    control={<Radio />} />
+                                <FormControlLabel 
+                                    value="pucks"
+                                    control={<Radio />} />
+                                <FormControlLabel 
+                                    value="pucks"
+                                    control={<Radio />} />
+                            </Stack>
+                            <Stack 
+                                direction="column"
+                                spacing={1.3}
+                                marginTop={.7} >
+                                <Typography variant='h6'>Pucks</Typography>
+                                <Typography variant='h6'>Shooter tutor</Typography>
+                                <Typography variant='h6'>Drinks</Typography>
+                            </Stack>
                         </Stack>
-                    </Item>
-                    <Box
-                        display='flex'
-                        flexDirection='column'
-                        alignItems='center'
-                        padding='5px'>
-                        <Typography>Skill level: { details[0].level ? 'Advanced' : 'Beginner' }</Typography>
-                        <Typography> { details[0].exposure ? 'Outdoor' : 'Indoor' } </Typography>
-                    {/* Mapping through RSVPs to get the names of attendees if a user is authenticated */}
-                    </Box>
-                    <Box
-                        display='flex'
-                        flexDirection='column'
-                        alignItems='center'
-                        padding='5px'>
-                        <Typography>Who's going:</Typography>
-                        {/* If no user is logged in, do this */}
-                        {!user.id && (
-                        <div>
-                            {/* Conditional rendering: if there's an "event_id" (i.e. an entry in the RSVP table),
-                            then this will show the details.length as an RSVP.  Otherwise it shows 0.  This is to 
-                            prevent the length being counted as an auto-rsvp because there is an entry for every event
-                            regardless of RSVPs. */}
-                            {!details[0].event_id ? (
-                                <Card
-                                sx={{  
-                                backgroundColor: '#baccde', 
-                                color: 'primary',
-                                margin:'auto', 
-                                minHeight: '10vh', 
-                                padding:'5px',
-                                textAlign: 'center',
-                                alignContent: 'center'
-                                }}>
-                                0 RSVP(s).
-                                <br />
-                                <Link href="/login">Log in</Link> to see more information.
-                            </Card>
-                            ) : (
-                                <Card
-                                sx={{  
-                                backgroundColor: '#baccde', 
-                                color: 'primary',
-                                margin:'auto', 
-                                minHeight: '10vh', 
-                                padding:'5px',
-                                textAlign: 'center',
-                                alignContent: 'center'
-                                }}>
-                                { details.length } RSVP(s).
-                                <br />
-                                {/* Added # to link here because it was redirecting Home instead without # */}
-                                <Link href="#/login">Log in</Link> to see more information.
-                            </Card>
-                            ) }  
-                        </div>
-                        )}
-                        {/* If a user is logged in, do this */}
-                        {user.id && (
-                            <div>
-                                {!details[0].event_id ? (
-                                <Card
-                                sx={{  
-                                backgroundColor: '#baccde', 
-                                color: 'primary',
-                                margin:'auto', 
-                                minHeight: '10vh', 
-                                padding:'5px',
-                                textAlign: 'center',
-                                alignContent: 'center'
-                                }}>
-                                No one has signed up yet.
-                            </Card>
-                            ) : (
-                                <Card
-                                sx={{  
-                                backgroundColor: '#baccde', 
-                                color: 'primary',
-                                margin:'auto', 
-                                minHeight: '10vh',
-                                minWidth: '50vw', 
-                                padding:'5px',
-                                textAlign: 'center',
-                                alignContent: 'center'
-                                }}>
-                                    {details.map(event => {
-                                        return (
-                                        <div key={event.user_id} >
-                                            <Typography variant="p1">{event.attendees}</Typography>
-                                        </div>
-                                        );
-                                    })}
-                                </Card>
-                            ) }
-                            </div>
-                        )}
                     </Box>
                     <Box
                         display='flex'
@@ -177,7 +116,16 @@ function RSVP(){
                                 textAlign: 'center',
                                 alignContent: 'center'
                                 }}>
-                            { details[0].notes }
+                            <TextField
+                                sx={{ backgroundColor: "fff" }}
+                                type="text"
+                                name="notes"
+                                variant="outlined"
+                                fullWidth
+                                label="Add notes"
+                                value={notes}
+                                onChange={(event) => setNotes(event.target.value)}
+                            />
                         </Card>
                     </Box>
                 </Grid>
